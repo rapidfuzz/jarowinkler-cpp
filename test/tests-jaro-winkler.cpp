a42e913c4354965545b39fe96ff18a49d09da1e9
Max Bachmann <kontakt@maxbachmann.de>
2022-06-06 20:40:03 +0200

    fix test compilation

--- test/tests-jaro-winkler.cpp
+++ test/tests-jaro-winkler.cpp
@@ -1,12 +1,15 @@
 #include <bitset>
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
 #include <jaro_winkler/jaro_winkler.hpp>
+
+using Catch::Approx;
 
 void validate_bitvector_word(const std::vector<int>& a, std::bitset<64> b)
 {
     std::bitset<64> bit_a(0);
-    for (int i = 0; i < a.size(); ++i)
+    for (size_t i = 0; i < a.size(); ++i)
     {
         bit_a[i] = a[i];
     }
@@ -17,20 +20,27 @@ void validate_bitvector_word(const std::vector<int>& a, std::bitset<64> b)
 struct FlaggedCharsOriginal {
     std::vector<int> P_flag;
     std::vector<int> T_flag;
-    size_t CommonChars;
+    int64_t CommonChars;
 };
 
 template <typename CharT1, typename CharT2>
-static inline FlaggedCharsOriginal flag_similar_characters_original(jaro_winkler::basic_string_view<CharT1> P,
-                                                                    jaro_winkler::basic_string_view<CharT2> T)
+size_t get_jaro_bound(const std::basic_string<CharT1>& P, const std::basic_string<CharT2>& T)
+{
+    size_t Bound = std::max(P.size(), T.size()) / 2;
+    if (Bound > 0) Bound--;
+    return Bound;
+}
+
+template <typename CharT1, typename CharT2>
+static inline FlaggedCharsOriginal flag_similar_characters_original(
+    const std::basic_string<CharT1>& P, const std::basic_string<CharT2>& T)
 {
     std::vector<int> P_flag(P.size() + 1);
     std::vector<int> T_flag(T.size() + 1);
 
-    size_t Bound = std::max(P.size(), T.size()) / 2;
-    if (Bound > 0) Bound--;
+    size_t Bound = get_jaro_bound(P, T);
 
-    size_t CommonChars = 0;
+    int64_t CommonChars = 0;
     for (size_t i = 0; i < T.size(); i++) {
         size_t lowlim = (i >= Bound) ? i - Bound : 0;
         size_t hilim = (i + Bound <= P.size() - 1) ? (i + Bound) : P.size() - 1;
@@ -49,13 +59,13 @@ static inline FlaggedCharsOriginal flag_similar_characters_original(jaro_winkler
 
 
 template <typename CharT1, typename CharT2>
-double jaro_similarity_original(jaro_winkler::basic_string_view<CharT2> P, jaro_winkler::basic_string_view<CharT1> T,
+double jaro_similarity_original(const std::basic_string<CharT1>& P, const std::basic_string<CharT2>& T,
                                 double score_cutoff)
 {
     auto flagged = flag_similar_characters_original(P, T);
 
     // Count the number of transpositions
-    size_t Transpositions = 0;
+    int64_t Transpositions = 0;
     size_t k = 0;
     for (size_t i = 0; i < T.size(); i++) {
         if (flagged.T_flag[i]) {
@@ -72,10 +82,11 @@ double jaro_similarity_original(jaro_winkler::basic_string_view<CharT2> P, jaro_
         }
     }
 
-    return jaro_winkler::common::result_cutoff(
-        jaro_winkler::detail::jaro_calculate_similarity(P, T, flagged.CommonChars, Transpositions), score_cutoff);
+    double sim = jaro_winkler::detail::jaro_calculate_similarity(
+        static_cast<int64_t>(P.size()), static_cast<int64_t>(T.size()),
+        flagged.CommonChars, Transpositions);
+    return jaro_winkler::common::result_cutoff(sim, score_cutoff);
 }
-
 
 /**
  * @name JaroWinklerFlagCharsTest
@@ -104,71 +115,45 @@ TEST_CASE("JaroWinklerTest")
         "karen"
     };
 
-    std::array<std::string, 1> long_string = {
-        "00000000000000000000000000000000000000000000000000000000000000000"
-    };
-
     SECTION("testFlagCharsWord")
     {
         for (const auto& name1 : names)
         {
-            jaro_winkler::string_view P(name1);
-            jaro_winkler::common::PatternMatchVector PM(P);
+            jaro_winkler::common::PatternMatchVector PM(name1.begin(), name1.end());
 
             for (const auto& name2 : names)
             {
-                jaro_winkler::string_view T(name2);
-
-                auto flagged_original = flag_similar_characters_original(P, T);
-                auto flagged_bitparallel = jaro_winkler::detail::flag_similar_characters_word(PM, P, T);
+                int Bound = static_cast<int>(get_jaro_bound(name1, name2));
+                auto flagged_original = flag_similar_characters_original(name1, name2);
+                auto flagged_bitparallel = jaro_winkler::detail::flag_similar_characters_word(PM,
+                    name1.begin(), name1.end(), name2.begin(), name2.end(), Bound);
 
                 INFO("Name1: " << name1 << ", Name2: " << name2);
 
                 validate_bitvector_word(flagged_original.P_flag, flagged_bitparallel.P_flag);
                 validate_bitvector_word(flagged_original.T_flag, flagged_bitparallel.T_flag);
-                REQUIRE(flagged_original.CommonChars == flagged_bitparallel.CommonChars);
+                REQUIRE(flagged_original.CommonChars == jaro_winkler::detail::count_common_chars(flagged_bitparallel));
             }    
         }
     }
 
+    // todo write test for blockwise implementation
     SECTION("testFlagCharsBlock")
     {
-        for (const auto& name1 : names)
-        {
-            jaro_winkler::string_view P(name1);
-            jaro_winkler::common::PatternMatchVector PM(P);
-
-            for (const auto& name2 : names)
-            {
-                jaro_winkler::string_view T(name2);
-
-                auto flagged_original = flag_similar_characters_original(P, T);
-                auto flagged_bitparallel = jaro_winkler::detail::flag_similar_characters_word(PM, P, T);
-
-                INFO("Name1: " << name1 << ", Name2: " << name2);
-
-                validate_bitvector_word(flagged_original.P_flag, flagged_bitparallel.P_flag);
-                validate_bitvector_word(flagged_original.T_flag, flagged_bitparallel.T_flag);
-                REQUIRE(flagged_original.CommonChars == flagged_bitparallel.CommonChars);
-            }    
-        }
     }
 
     SECTION("testFullResult")
     {
         for (const auto& name1 : names)
         {
-            jaro_winkler::string_view P(name1);
-
             for (const auto& name2 : names)
             {
-                jaro_winkler::string_view T(name2);
-
-                double Sim_original = jaro_similarity_original(P, T, 0);
-                double Sim_bitparallel = jaro_winkler::detail::jaro_similarity_word(P, T, 0);
+                double Sim_original = jaro_similarity_original(name1, name2, 0);
+                double Sim_bitparallel = jaro_winkler::detail::jaro_similarity(
+                    name1.begin(), name1.end(), name2.begin(), name2.end(), 0);
 
                 INFO("Name1: " << name1 << ", Name2: " << name2);
-                REQUIRE(Sim_original == Sim_bitparallel);
+                REQUIRE(Sim_original == Approx(Sim_bitparallel));
             }    
         }
     }
@@ -177,14 +162,11 @@ TEST_CASE("JaroWinklerTest")
     {
         for (const auto& name1 : names)
         {
-            jaro_winkler::string_view P(name1);
-
             for (const auto& name2 : names)
             {
-                jaro_winkler::string_view T(name2);
-
-                double Sim_original = jaro_similarity_original(P, T, 0.9);
-                double Sim_bitparallel = jaro_winkler::detail::jaro_similarity_word(P, T, 0.9);
+                double Sim_original = jaro_similarity_original(name1, name2, 0.9);
+                double Sim_bitparallel = jaro_winkler::detail::jaro_similarity(
+                    name1.begin(), name1.end(), name2.begin(), name2.end(), 0.9);
 
                 INFO("Name1: " << name1 << ", Name2: " << name2);
                 REQUIRE(Sim_original == Sim_bitparallel);
